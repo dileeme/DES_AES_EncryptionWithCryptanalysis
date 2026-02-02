0b7dcdbd6787b3b2fed93be80b4c9c2a7e133047
dilen shankar <elchurrocuandotebese@gmail.com>
2026-02-02 16:12:59 +0530

    Update aes.h

--- AES/aes.h
+++ AES/aes.h
@@ -1,1 +1,20 @@
+#ifndef AES_H
+#define AES_H
+
+#define AES_BLOCK_SIZE 16
+#define AES_KEY_SIZE   16
+
+#define ENCRYPTION_MODE 1
+#define DECRYPTION_MODE 0
+
+typedef unsigned char uint8;
+
+/* Key schedule */
+void aes_key_expansion(uint8 *key, uint8 *round_keys);
+
+/* Block operations */
+void aes_encrypt_block(uint8 *input, uint8 *output, uint8 *round_keys);
+void aes_decrypt_block(uint8 *input, uint8 *output, uint8 *round_keys);
+
+#endif
 
